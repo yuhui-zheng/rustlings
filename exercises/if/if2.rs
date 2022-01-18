9fcb5225f7f6a4741899359b38e5efbd5afd929b
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2022-01-18 12:22:26 -0800

    exercise: if

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -9,8 +9,11 @@
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } else if fizzish == "fuzz" {
+        "bar"
+    }
+    else {
+        "baz"
     }
 }
 
